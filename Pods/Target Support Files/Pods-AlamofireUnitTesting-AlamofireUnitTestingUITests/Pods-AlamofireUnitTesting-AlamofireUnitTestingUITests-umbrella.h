2adbcf178467a6f3da7bc91e1cb0a63dd400b57a
IlliasBaraka <hassanbaraka@mail.usf.edu>
2021-05-24 18:13:04 -0400

    v2

---
+++ Pods/Target Support Files/Pods-AlamofireUnitTesting-AlamofireUnitTestingUITests/Pods-AlamofireUnitTesting-AlamofireUnitTestingUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AlamofireUnitTesting_AlamofireUnitTestingUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AlamofireUnitTesting_AlamofireUnitTestingUITestsVersionString[];
+
